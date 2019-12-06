5334a9ab1922dfdb771185714912e61c60653312
Gunnar Þór Magnússon <gunnar@magnusson.io>
2019-12-06 11:30:54 +0100

    Solve part b

--- 2019/src/06.py
+++ 2019/src/06.py
@@ -6,8 +6,48 @@ def partA(connections):
     hs = heights(orbits)
     return sum(hs.values())
 
-def partB(codes):
-    pass
+def partB(connections):
+    orbits = tree(connections)
+
+    orbits_you = orbits_of(orbits, 'YOU')
+    orbits_san = orbits_of(orbits, 'SAN')
+    common = first_common(orbits_you, orbits_san)
+    new = prune(orbits, common)
+    hs = heights(new)
+
+    return hs['YOU'] + hs['SAN'] - 2
+
+def prune(orbits, source):
+    new = {}
+    for sat in orbits:
+        objs = set(orbits_of(orbits, sat))
+        if source in objs:
+            new[sat] = orbits[sat]
+
+    return new
+
+def first_common(orbits_a, orbits_b):
+    orbits_a.reverse()
+    orbits_b.reverse()
+    assert orbits_a[0] == orbits_b[0]
+    for a, b in zip(orbits_a, orbits_b):
+        if a == b:
+            pt = a
+        else:
+            return pt
+
+def orbits_of(orbits, pt):
+    orbs = []
+
+    while True:
+        source = orbits.get(pt, None)
+        if source is not None:
+            orbs.append(source)
+            pt = source
+        else:
+            break
+
+    return orbs
 
 def tree(connections):
     orbits = {}
@@ -35,7 +75,7 @@ def heights(orbits):
 
 class TestProblem(unittest.TestCase):
 
-    def test_run1(self):
+    def test_run2(self):
         lines = """COM)B
 B)C
 C)D
@@ -48,9 +88,25 @@ E)J
 J)K
 K)L""".split('\n')
         conns = [line.strip().split(')') for line in lines]
-        orbits = tree(conns)
-        hs = heights(orbits)
         self.assertEqual(partA(conns), 42)
+
+    def test_run2(self):
+        liness = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN""".split('\n')
+        connss = [line.strip().split(')') for line in liness]
+        self.assertEqual(partB(connss), 4)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
