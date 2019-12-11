563fa827f6741bf087a11f5f178e50c42b36d192
Gunnar Þór Magnússon <gunnar@magnusson.io>
2019-12-11 09:41:44 +0100

    WIP part b

--- 2019/src/11.py
+++ 2019/src/11.py
@@ -30,56 +30,91 @@ class Direction(enum.Enum):
 def partA(codes):
     instructions = []
     vm = VM(codes, [0])
+
+    grid = {}
+    pos_x = 0
+    pos_y = 0
+    facing = Direction.N
+
     while True:
         vm.run()
         if len(vm._output) > 2:
             import pdb; pdb.set_trace()
             raise Exception("Got too much output")
         elif len(vm._output) == 2:
-            instructions.append(vm._output)
-            vm.inputs = [vm._output[0]]
+            paint, move = vm._output
+            grid[(pos_x, pos_y)] = paint
+
+            facing = facing.turn(move)
+            x, y = facing.step()
+
+            pos_x += x
+            pos_y += y
+
+            vm.inputs = [grid.get((pos_x, pos_y), 0)]
             vm._output = []
         else:
             break
 
-    return paint(instructions)
-
-def paint(instructions):
-    grid = {(0, 0)}
-    pos_x = 0
-    pos_y = 0
-    facing = Direction.N
-    for _, move in instructions:
-        x, y = facing.step()
-        facing = facing.turn(move)
-        pos_x += x
-        pos_y += y
-        grid.add((pos_x, pos_y))
-
     return len(grid)
 
 def partB(codes):
-    vm = VM(codes, [2])
-    vm.run()
-    return vm.output
+    instructions = []
+    vm = VM(codes, [1])
+
+    grid = {}
+    pos_x = 0
+    pos_y = 0
+    facing = Direction.N
+
+    while True:
+        vm.run()
+        if len(vm._output) > 2:
+            import pdb; pdb.set_trace()
+            raise Exception("Got too much output")
+        elif len(vm._output) == 2:
+            paint, move = vm._output
+            grid[(pos_x, pos_y)] = paint
+
+            facing = facing.turn(move)
+            x, y = facing.step()
+
+            pos_x += x
+            pos_y += y
+
+            vm.inputs = [grid.get((pos_x, pos_y), 0)]
+            vm._output = []
+        else:
+            break
+    y_m = min(pt[1] for pt in grid)
+    grid = {(pt[0], pt[1] - y_m) for pt, paint in grid.items() if paint}
+
+    x_m, x_M = min(pt[0] for pt in grid), max(pt[0] for pt in grid)
+    y_m, y_M = min(pt[1] for pt in grid), max(pt[1] for pt in grid)
+
+    width = x_M - x_m + 1
+    height = y_M - y_m + 1
+    floor = [[0] * width] * height
+
+    with open('pts.txt', 'w') as fh:
+        for x, y in grid:
+            print('{},{}'.format(x,y), file=fh)
+
+    for x, y in grid:
+        floor[y - y_m][x - x_m] = 1
+
+    #import pdb; pdb.set_trace()
+    for row in floor:
+        for pt in row:
+            out = 'x' if pt else ' '
+            print(out, end='')
+        print('|\n', end='')
 
 
 class TestProblem(unittest.TestCase):
 
     def test_run0(self):
-        ins = [
-            [1, 0],
-            [0, 0],
-            [1, 0],
-            [1, 0],
-            [0, 1],
-            [1, 0],
-            [1, 0],
-        ]
-        exp = 6
-        got = paint(ins)
-        assert got == exp
-
+        pass
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -90,4 +125,4 @@ if __name__ == '__main__':
         codes = [int(i) for i in fh.read().strip().split(',')]
 
     print(partA(codes))
-    #print(partB(codes))
+    partB(codes)
