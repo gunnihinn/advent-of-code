520f3260a171be3e6048bc5514ab50051bc2d2fb
Gunnar Þór Magnússon <gunnarm@tern.is>
2022-12-11 10:12:10 +0000

    Init day 11

--- 2022/scripts/init.sh
+++ 2022/scripts/init.sh
@@ -8,3 +8,6 @@ day=$(seq -f "%02g" $1 $1)
 mkdir -p "$root/$day"
 cp "$root/main.cpp" "$root/$day"
 echo "add_executable($day $day/main.cpp)" >>"$root/CMakeLists.txt"
+
+mv "~/Downloads/ex*" "$root/$day"
+mv "~/Downloads/input" "$root/$day/input.txt"
