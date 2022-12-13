8e06b30ee2ea8bf4c7a908f18296f603bb2f85fc
Gunnar Þór Magnússon <gunnarm@tern.is>
2022-12-13 15:16:33 +0000

    Parse input

--- 2022/13/main.cpp
+++ 2022/13/main.cpp
@@ -1,5 +1,7 @@
+#include <cassert>
 #include <algorithm>
 #include <fstream>
+#include <optional>
 #include <iostream>
 #include <numeric>
 #include <sstream>
@@ -8,13 +10,107 @@
 
 using namespace std;
 
+class Packet
+{
+public:
+    vector< Packet > children;
+    optional< int > val;
+
+    string str()
+    {
+        stringstream ss;
+        if( val )
+        {
+            ss << val.value();
+        }
+        else if( children.empty() )
+        {
+            ss << "[]";
+        }
+        else
+        {
+            ss << "[";
+            for(unsigned long int i = 0; i < children.size() - 1; i++)
+            {
+                ss << children[ i ].str() << ",";
+            }
+            ss << children.back().str() << "]";
+        }
+        return ss.str();
+    }
+
+    static pair< optional< Packet >, string > parse( string line )
+    {
+        if( line == "" )
+        {
+            return { {}, "" };
+        }
+        else if( line.at( 0 ) == '[' )
+        {
+            int c = 1;
+            int i = 1;
+            while( c > 0 )
+            {
+                assert( i < line.size() );
+                if( line.at( i ) == '[' )
+                    c++;
+                else if( line.at( i ) == ']' )
+                    c--;
+                i++;
+            }
+
+            Packet packet;
+            string objs = line.substr( 1, i - 2 );
+            for(auto res = Packet::parse( objs ); res.first; res = Packet::parse( res.second ) )
+            {
+                packet.children.push_back( res.first.value() );
+            }
+
+            if( i >= line.size() )
+            {
+                return { packet, "" };
+            }
+            else
+            {
+                return { packet, line.substr( i + 1, line.size() ) };
+            }
+        }
+        else
+        {
+            int i = 0;
+            while( i < line.size() && '0' <= line.at( i ) && line.at( i ) <= '9'  )
+                i++;
+            Packet packet;
+            packet.val = stoi( line.substr( 0, i ) );
+
+            if( i == line.size() )
+            {
+                return { packet, "" };
+            }
+            else
+            {
+                assert( i + 1 < line.size() );
+                return { packet, line.substr( i + 1, line.size() ) };
+            }
+        }
+    }
+};
+
 class Data
 {
+public:
+    vector< pair< Packet, Packet > > lines;
 };
 
 int part1( Data data )
 {
     int result = 0;
+    for(auto pp : data.lines)
+    {
+        cout << pp.first.str() << "\n";
+        cout << pp.second.str() << "\n";
+        cout << "\n";
+    }
     return result;
 }
 
@@ -26,7 +122,36 @@ int part2( Data data )
 
 Data parse( istream& is )
 {
+    vector< string > lines;
+    for(string line; getline( is, line );)
+    {
+        if( line != "" )
+        {
+            lines.push_back( line );
+        }
+    }
+
     auto data = Data();
+    for(auto it = lines.begin(); it != lines.end(); ++it)
+    {
+        auto [ left, rest1 ] = Packet::parse( *it );
+        if( !rest1.empty() )
+        {
+            cout << "line: " << *it << "\n";
+            cout << "rest: " << rest1 << "\n";
+        }
+
+        ++it;
+        auto [ right, rest2 ] = Packet::parse( *it );
+        if( !rest2.empty() )
+        {
+            cout << "line: " << *it << "\n";
+            cout << "rest: " << rest2 << "\n";
+        }
+
+        data.lines.push_back( { left.value(), right.value() } );
+    }
+
     return data;
 }
 
